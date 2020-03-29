4b0a86047e0635fcadbc35887890a208f5cba32b
Estelle Saget <estelle.saget@live.fr>
2020-03-29 18:16:20 +0200

    back routes, seeds, controller ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  resources :projets, only: [ :index, :show ]
+
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-
-  resources :projets
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Projet.destroy_all
+
+puts "Creating projets..."
+
+guesswhat = Projet.new(name: "Guess What")
+mistercocktail = Projet.new(name: "Mister Cocktail")
+petme = Projet.new(name: "Pet me")
+
+[ guesswhat, mistercocktail, petme ].each do |projet|
+  projet.save
+  puts "Created #{projet.name}"
+end
+puts "Finished!"
